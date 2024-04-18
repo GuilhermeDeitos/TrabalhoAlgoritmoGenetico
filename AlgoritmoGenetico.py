f0c6a962e6296549de50db829bd2c5f45dbc2451
petcompgroup <petcomp.cascavel@gmail.com>
2024-04-18 16:55:51 -0300

    Erro na seleção

--- AlgoritmoGenetico.py
+++ AlgoritmoGenetico.py
@@ -23,11 +23,42 @@ class AlgoritmoGenetico:
        
     
     def converteBinToFloat(self, binario):
-        return int(binario, 2)
+        inteiro,dec = binario.split('.')        
+        inteiro = int(inteiro,2)
+        dec = int(dec, 2)
+        result = inteiro + dec/(10**len(str(dec)))
+        return result
     
-    def converteFloatToBin(self, numero):
-        return bin(numero)
-    
+    def converteFloatToBin(self, numero, casas=10):
+        # Converte a parte inteira
+        parte_inteira = int(numero)
+        parte_inteira_bin = bin(parte_inteira).replace("0b", "")
+
+        # Converte a parte decimal
+        parte_decimal = numero - parte_inteira
+        parte_decimal_bin = ""
+        while casas:
+            parte_decimal *= 2
+            bit = int(parte_decimal)
+
+            if bit == 1:
+                parte_decimal -= bit
+                parte_decimal_bin += '1'
+            else:
+                parte_decimal_bin += '0'
+
+            casas -= 1
+
+        # Combina as partes inteira e decimal
+        numero_bin = parte_inteira_bin + "." + parte_decimal_bin
+        print(f"Binario: {numero_bin} Decimal: {numero}")
+        return numero_bin
+
+    def decimal_converter(self,num): 
+        while num > 1:
+            num /= 10
+        return num
+        
     
     def calcFitness(self, x, y):
         fitness = self.funcaoBase(x, y)
@@ -38,8 +69,9 @@ class AlgoritmoGenetico:
     
     def gerarPopulacaoInicial(self):
         for i in range(self.tamanho_populacao):
-            x = random.randint(0, 15)
-            y = random.randint(0, 10)
+            #gerar um float aleatorio x
+            x = round(random.uniform(0, 15), 3)
+            y = round(random.uniform(0, 10), 3)
             individuo = {}
             individuo["x"] = self.converteFloatToBin(x)
             individuo["y"] = self.converteFloatToBin(y)
@@ -59,36 +91,37 @@ class AlgoritmoGenetico:
         while(self.maximoGeracoes > self.geracao):
             somaFitness = sum([i["fitness"] for i in self.populacao])
             for i in self.populacao:
-                i["porcentagem"] = i["fitness"]/somaFitness
+                print("Fitness",i["fitness"], somaFitness)
+                i["porcentagem"] = i["fitness"]/(somaFitness + 1e-9)  # Adiciona uma pequena constante para evitar divisão por zero
+            
+            print("Porcentagem: ",i)
 
             #Agora selecionar para realizar o cruzamento
-            individuos = self.populacao
+            individuos = self.populacao.copy()
             novaPopulacao = []
             for i in range(self.tamanho_populacao):
                 isElitismo = random.random() < self.taxaElitismo
                 if isElitismo:
+                    if not individuos:
+                        break
                     individuo = max(individuos, key=lambda x:x["fitness"])
                     novaPopulacao.append(individuo)
                     individuos.remove(individuo)
                 else:
-                    if(len(individuos) == 0):
-                        break
-                    individuo1 = random.choices(individuos, weights=[i["porcentagem"] for i in individuos])[0]
-                    individuo2 = random.choices(individuos, weights=[i["porcentagem"] for i in individuos])[0]
-                    while(individuo1 == individuo2):
-                        individuo2 = random.choices(individuos, weights=[i["porcentagem"] for i in individuos])[0]
+                    individuosRestantes = individuos.copy()
+                    for _ in range(len(individuos) // 2):
+                        individuo1 = random.choices(individuosRestantes, weights=[i["porcentagem"] for i in individuosRestantes])[0]
+                        individuosRestantes.remove(individuo1)
+                        individuo2 = random.choices(individuosRestantes, weights=[i["porcentagem"] for i in individuosRestantes])[0]
+                        individuosRestantes.remove(individuo2)
+                        filho1, filho2 = self.cruzamento(individuo1, individuo2)
+                        novaPopulacao.append(self.mutacao(filho1))
+                        novaPopulacao.append(self.mutacao(filho2))
                         
-                    filho1, filho2 = self.cruzamento(individuo1, individuo2)
-                    novaPopulacao.append(self.mutacao(filho1))
-                    novaPopulacao.append(self.mutacao(filho2))
-                    individuos.remove(individuo1)
-                    individuos.remove(individuo2)
-                    
             self.populacao = novaPopulacao
             self.geracao += 1            
             print(f"População Após cruzamento da {self.geracao} geração: ")
             self.printIndividuos(novaPopulacao)
-        self.plotarGrafico()
         
         
     
@@ -100,40 +133,37 @@ class AlgoritmoGenetico:
         return individuo
         
     def inversaoBinaria(self, individuo):
-        print(individuo)
-        signal,b = individuo["x"].split("b")
-        c = random.randint(0,len(b)-1)
-    
-        if b[c] == "0":
-            b = b[:c] + "1" + b[c+1:]
-        else:
-            b = b[:c] + "0" + b[c+1:]
-        individuo["x"] = signal + "b" + b
-        signal,b = individuo["y"].split("b")
-        c = random.randint(0,len(b)-1)
-        if b[c] == "0":
-            b = b[:c] + "1" + b[c+1:]
-        else:
-            b = b[:c] + "0" + b[c+1:]
+        inteiro, dec = individuo["x"].split(".")
+        inteiro, dec = self.auxInversao(list(inteiro)), self.auxInversao(list(dec))
+        inteiro, dec = individuo["y"].split(".")
+        inteiro, dec = self.auxInversao(list(inteiro)), self.auxInversao(list(dec))
         
-        individuo["y"] = signal + "b" + b
+        individuo["y"] = "".join(inteiro) + "." + "".join(dec)
+        individuo["x"] = "".join(inteiro) + "." + "".join(dec)
+        
         individuo["fitness"] = self.calcFitness(self.converteBinToFloat(individuo["x"]), self.converteBinToFloat(individuo["y"]))
+        print("Cruzamento nulo ",individuo["fitness"] == 0)
         return individuo
      
-                
+    def auxInversao(self, listIndividuo):
+        return ['1' if i == '0' else '0' for i in listIndividuo]
             
                    
         
     def cruzamento(self, individuo1:dict, individuo2:dict): #Cruzamento por 2 pontos aleatorios
-        ponto1 = random.randint(0, len(individuo1["x"]))
-        ponto2 = random.randint(ponto1, len(individuo1["x"]))
-        filho1 = {}
-        filho2 = {}
-        filho1["x"] = individuo1["x"][:ponto1] + individuo2["x"][ponto1:ponto2] + individuo1["x"][ponto2:]
-        filho1["y"] = individuo1["y"][:ponto1] + individuo2["y"][ponto1:ponto2] + individuo1["y"][ponto2:]
-        filho2["x"] = individuo2["x"][:ponto1] + individuo1["x"][ponto1:ponto2] + individuo2["x"][ponto2:]
-        filho2["y"] = individuo2["y"][:ponto1] + individuo1["y"][ponto1:ponto2] + individuo2["y"][ponto2:]
-        
+        filho1 = {"x": "", "y": ""}
+        filho2 = {"x": "", "y": ""}
+
+        for coord in ["x", "y"]:
+            parte_inteira1, parte_decimal1 = individuo1[coord].split('.')
+            parte_inteira2, parte_decimal2 = individuo2[coord].split('.')
+
+            ponto_corte_inteiro = random.randint(1, len(parte_inteira1) - 1) if len(parte_inteira1) > 1 else 0
+            ponto_corte_decimal = random.randint(1, len(parte_decimal1) - 1) if len(parte_decimal1) > 1 else 0
+
+            filho1[coord] = parte_inteira1[:ponto_corte_inteiro] + parte_inteira2[ponto_corte_inteiro:] + '.' + parte_decimal1[:ponto_corte_decimal] + parte_decimal2[ponto_corte_decimal:]
+            filho2[coord] = parte_inteira2[:ponto_corte_inteiro] + parte_inteira1[ponto_corte_inteiro:] + '.' + parte_decimal2[:ponto_corte_decimal] + parte_decimal1[ponto_corte_decimal:]
+
         filho1["fitness"] = self.calcFitness(self.converteBinToFloat(filho1["x"]),self.converteBinToFloat(filho1["y"]))
         filho2["fitness"] = self.calcFitness(self.converteBinToFloat(filho2["x"]),self.converteBinToFloat(filho2["y"]))
         return filho1, filho2
@@ -150,17 +180,18 @@ class AlgoritmoGenetico:
 
         return listaFinal
     
-    def plotarGrafico(self):
+    def plotarGrafico3d(self):
         populacaoFloat = []
         for i in self.populacao:
             if self.melhor_solucao == None or i["fitness"] > self.melhor_solucao["fitness"]:
                 self.melhor_solucao = i
                 self.melhor_solucao_geracao = self.geracao
+            print(i)
             populacaoFloat.append({"x":self.converteBinToFloat(i["x"]), "y":self.converteBinToFloat(i["y"]), "fitness":i["fitness"]})
             
         print("População: ", populacaoFloat)
         
-            
+         
         plotIndList = self.getListOfDict(populacaoFloat)
         plt.style.use('_mpl-gallery')
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
@@ -172,7 +203,24 @@ class AlgoritmoGenetico:
         fig.set_size_inches(10, 10)
 
         plt.show()
-    
+    def plotarGrafico2d(self):
+        populacaoFloat = []
+
+        for i in self.populacao:
+            populacaoFloat.append({"x":self.converteBinToFloat(i["x"]), "y":self.converteBinToFloat(i["y"]), "fitness":i["fitness"]})
+
+        plotIndList = self.getListOfDict(populacaoFloat)
+
+        X, Y = np.meshgrid(np.array(plotIndList[0]), np.array(plotIndList[1]))
+        calcFitness_vect = np.vectorize(self.calcFitness)
+        Z = calcFitness_vect(X, Y)
+
+        plt.imshow(Z, cmap='hot', interpolation='nearest')
+        plt.colorbar(label='Fitness')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+        
     def printIndividuos(self, individuos:list):
         df = pd.DataFrame(individuos)
         print(df)
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from math import sqrt, sin
 
 
 funcaoBase = lambda x,y: sin(x) + sqrt(x) - (y/3)
-print(funcaoBase(15,0))
-algGen = AlgoritmoGenetico(tamanho_populacao=10, funcaoBase=funcaoBase, maximoGeracoes=5)
+algGen = AlgoritmoGenetico(tamanho_populacao=10, funcaoBase=funcaoBase, maximoGeracoes=50)
 
 algGen.gerarPopulacaoInicial()
 algGen.selecaoRoleta()
-algGen.plotarGrafico()
+algGen.plotarGrafico3d()
+algGen.plotarGrafico2d()
