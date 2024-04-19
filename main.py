43feb45e9120998be4b0fd945298beb36d36da8d
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2024-04-18 23:15:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from math import sqrt, sin
 
 
 funcaoBase = lambda x,y: sin(x) + sqrt(x) - (y/3)
-algGen = AlgoritmoGenetico(tamanho_populacao=10, funcaoBase=funcaoBase, maximoGeracoes=50)
+algGen = AlgoritmoGenetico(tamanho_populacao=10, funcaoBase=funcaoBase, maximoGeracoes=100)
 
 algGen.gerarPopulacaoInicial()
 algGen.selecaoRoleta()
 algGen.plotarGrafico3d()
 algGen.plotarGrafico2d()
+algGen.exibirMelhorSolucao()
